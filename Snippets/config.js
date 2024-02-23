8e72167cf7749a5467385284028109b40cd3a56b
Ryan <116788103+Aozndev@users.noreply.github.com>
2024-02-23 11:38:41 -0300

    Add files via upload

---
+++ Snippets/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    token: 'token do seu bot aqui',
+
+    mongodb: 'token da sua mongodb',
+    mongoName: 'nome que vc quer para a pasta do seu mongodb'
+
+                 }
